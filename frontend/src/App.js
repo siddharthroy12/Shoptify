3225ead6ef9a3cd4377d480f5eac5357a194c6d8
Siddharth Roy <siddharthroy36912@gmail.com>
2020-11-17 16:55:36 +0530

    Backend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
 function App() {
   return (
-    <>
-      <h1>Welcome to shop</h1>
-    </>
+    <Router>
+      <Header></Header>
+        <main className='py-3'>
+          <Container>
+           <Route path='/' component={HomeScreen} exact></Route>
+           <Route path='/product/:id' component={ProductScreen}></Route>
+          </Container>
+        </main>
+      <Footer></Footer>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,32 @@
+import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+function Header() {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
+                <Container>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>Shoptify</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <LinkContainer to="/cart">
+                            <Nav.Link >
+                                <i className='fas fa-shopping-cart' /> Cart
+                            </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                            <Nav.Link >
+                                <i className='fas fa-user' /> Sign In
+                            </Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
