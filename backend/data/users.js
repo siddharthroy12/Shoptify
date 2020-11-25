9a7645986fe685392d45e94e4661bff993754145
Siddharth Roy <siddharthroy36912@gmail.com>
2020-11-25 16:43:46 +0530

    Added Readme and Screenshot

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,18 +4,18 @@ const users = [
     {
         name: 'Admin User',
         email: 'admin@example.com',
-        password: bcrypt.hashSync('Aezakmi1', 10),
+        password: bcrypt.hashSync('1234', 10),
         isAdmin: true
     },
     {
         name: 'Vansh',
         email: 'vansh@example.com',
-        password: bcrypt.hashSync('Aezakmi1', 10),
+        password: bcrypt.hashSync('1234', 10),
     },
     {
         name: 'Saif',
         email: 'saif@example.com',
-        password: bcrypt.hashSync('Aezakmi1', 10),
+        password: bcrypt.hashSync('1234', 10),
     }
 ]
 
